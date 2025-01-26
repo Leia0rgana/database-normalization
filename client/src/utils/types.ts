c39b5d2f9949c9d6aeb88020fbbe07a2842f78f6
Natalya Malyushenko <natamalyushenko@gmail.com>
2025-01-26 14:18:26 +0300

    [added] types for tableInfo entity

---
+++ client/src/utils/types.ts
@@ -0,0 +1,18 @@
+export type Attribute = {
+  name: string;
+  isPrimaryKey: boolean;
+  foreignKeyReference?: ForeignKeyReference;
+};
+
+export type ForeignKeyReference = {
+  tableName: string;
+  attributeName: string;
+  relationshipType?: RelationshipType;
+};
+
+export type RelationshipType = '1-1' | '1-N' | 'N-M';
+
+export type FunctionalDependency = {
+  determinant: string[];
+  dependent: string[];
+};
