92ca51e5a08d26b4c55656f1014e39a8de35e799
Natalya Malyushenko <natamalyushenko@gmail.com>
2025-05-09 15:31:58 +0300

    [added] routes

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,11 +4,36 @@ import './index.css';
 import App from './App.tsx';
 import { store } from './redux/store';
 import { Provider } from 'react-redux';
+import { createBrowserRouter, RouterProvider } from 'react-router';
+import { PageNotFound } from './components/routes/PageNotFound';
+import { MainLayout } from './components/routes/MainLayout';
+import { Home } from './components/routes/Home';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    Component: MainLayout,
+    children: [
+      {
+        index: true,
+        Component: Home,
+      },
+      {
+        path: '/auth',
+        Component: App,
+      },
+    ],
+  },
+  {
+    path: '*',
+    Component: PageNotFound,
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router} />
     </Provider>
   </StrictMode>
 );
