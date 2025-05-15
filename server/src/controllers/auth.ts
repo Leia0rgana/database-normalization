a581ce3b6faacc5219eab39087246941028800f2
Natalya Malyushenko <natamalyushenko@gmail.com>
2025-05-16 00:10:20 +0300

    [added] EPs with user verification

--- server/src/controllers/auth.ts
+++ server/src/controllers/auth.ts
@@ -1,10 +1,10 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { Request, Response } from 'express';
-import 'dotenv/config';
 import bcrypt from 'bcryptjs';
 import jwt, { Secret } from 'jsonwebtoken';
 import userModel from '../models/user';
 import { COOKIE_OPTIONS } from '../utils/constants';
+import { transporter } from '../config/nodemailer';
 
 // TODO TYPE ERROR
 
@@ -37,6 +37,17 @@ export const signUp = async (req: Request, res: Response) => {
     });
 
     res.cookie('token', token, COOKIE_OPTIONS);
+
+    //sending welcome mail
+
+    const mailOptions = {
+      from: process.env.SENDER_EMAIL,
+      to: email,
+      subject: 'Добро пожаловать в DBNormalization',
+      text: `Добро пожаловать в DBNormalization! Ваш аккаунт создан с использованием адреса электронной почты: ${email}`,
+    };
+
+    await transporter.sendMail(mailOptions);
 
     return res.json({ success: true });
   } catch (error: any) {
@@ -82,6 +93,83 @@ export const logout = async (req: Request, res: Response) => {
   try {
     res.clearCookie('token');
     return res.json({ success: true, message: 'Logged out' });
+  } catch (error: any) {
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export const sendVerifyOtp = async (req: Request, res: Response) => {
+  try {
+    const { userId } = req.body;
+
+    const user = await userModel.findById(userId);
+
+    if (!user) {
+      return res.json({ success: false, message: 'User not found' });
+    }
+
+    if (user.isAccountVerified) {
+      return res.json({ success: false, message: 'Account already verified' });
+    }
+
+    const otp = String(Math.floor(100000 + Math.random() * 900000));
+
+    user.verifyOtp = otp;
+    user.verifyOtpExpireAt = Date.now() + 24 * 60 * 60 * 1000;
+
+    await user.save();
+
+    const mailOptions = {
+      from: process.env.SENDER_EMAIL,
+      to: user.email!,
+      subject: 'Подтверждение аккаунта | DBNormalization',
+      text: `Ваш код для подтверждения аккаунта: ${otp}. Код действителен 24 часа.`,
+    };
+
+    await transporter.sendMail(mailOptions);
+  } catch (error: any) {
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export const verifyEmail = async (req: Request, res: Response) => {
+  const { userId, otp } = req.body;
+
+  if (!userId || !otp) {
+    return res.json({ success: false, message: 'Missing details' });
+  }
+
+  try {
+    const user = await userModel.findById(userId);
+
+    if (!user) {
+      return res.json({ success: false, message: 'User not found' });
+    }
+
+    if (user.verifyOtp === '' || user.verifyOtp !== otp) {
+      return res.json({ success: false, message: 'Invalid verification code' });
+    }
+
+    if (user.verifyOtpExpireAt < Date.now()) {
+      return res.json({ success: false, message: 'Verification code expired' });
+    }
+
+    user.isAccountVerified = true;
+
+    user.verifyOtp = '';
+    user.verifyOtpExpireAt = 0;
+
+    await user.save();
+
+    return res.json({ success: true, message: 'Succesful email verification' });
+  } catch (error: any) {
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export const isAuthenticated = async (req: Request, res: Response) => {
+  try {
+    return res.json({ success: true });
   } catch (error: any) {
     res.json({ success: false, message: error.message });
   }
--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,8 +1,19 @@
 import express from 'express';
-import { login, logout, signUp } from '../controllers/auth';
+import {
+  isAuthenticated,
+  login,
+  logout,
+  sendVerifyOtp,
+  signUp,
+  verifyEmail,
+} from '../controllers/auth';
+import { userAuth } from '../middleware/userAuth';
 
 export const authRouter = express.Router();
 
-authRouter.post('/signup', signUp);
+authRouter.post('/sign-up', signUp);
 authRouter.post('/login', login);
 authRouter.post('/logout', logout);
+authRouter.post('/send-verify-otp', userAuth, sendVerifyOtp);
+authRouter.post('/verify-account', userAuth, verifyEmail);
+authRouter.post('/is-auth', userAuth, isAuthenticated);
