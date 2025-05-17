dfe56cd926c7ab1dfdac3eaea29fc0130a82519e
Natalya Malyushenko <natamalyushenko@gmail.com>
2025-05-17 19:29:59 +0300

    [changed] http method

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -16,4 +16,4 @@ authRouter.post('/login', login);
 authRouter.post('/logout', logout);
 authRouter.post('/send-verify-otp', userAuth, sendVerifyOtp);
 authRouter.post('/verify-account', userAuth, verifyEmail);
-authRouter.post('/is-auth', userAuth, isAuthenticated);
+authRouter.get('/is-auth', userAuth, isAuthenticated);
