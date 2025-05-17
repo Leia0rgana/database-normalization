bd5720129b822d4b7e60a707a99ff11f39430f85
Natalya Malyushenko <natamalyushenko@gmail.com>
2025-05-17 19:29:35 +0300

    [added] allowed origins

--- server/src/index.ts
+++ server/src/index.ts
@@ -11,9 +11,11 @@ import { userRouter } from './routes/user';
 const app: Express = express();
 const port = process.env.PORT;
 
+const allowedOrigins = ['http://localhost:5173'];
+
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ credentials: true }));
+app.use(cors({ origin: allowedOrigins, credentials: true }));
 app.use(morgan('dev'));
 
 if (!port) {
