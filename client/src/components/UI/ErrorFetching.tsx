d1cfc9d5c7e5d383d46046ea7d58623776bba4ce
Natalya Malyushenko <natamalyushenko@gmail.com>
2025-05-17 19:36:28 +0300

    [added] error component

---
+++ client/src/components/UI/ErrorFetching.tsx
@@ -0,0 +1,17 @@
+import { Link } from 'react-router';
+
+export const ErrorFetching = () => {
+  return (
+    <div className="flex flex-col items-center my-24 gap-1.5 text-[#252b35]">
+      <h1 className="flex items-center text-3xl font-semibold gap-1.5">
+        Ошибка при загрузке данных <span className="text-4xl">&#128547;</span>
+      </h1>
+      <Link
+        to="/"
+        className="underline hover:scale-101 transition-transform duration-300 ease-in-out"
+      >
+        Перейти на главную
+      </Link>
+    </div>
+  );
+};
