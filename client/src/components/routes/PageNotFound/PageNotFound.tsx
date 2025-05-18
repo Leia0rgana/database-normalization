9188823fb280e16265b14f8a36884e803666c40b
Natalya Malyushenko <natamalyushenko@gmail.com>
2025-05-18 16:07:30 +0300

    [changed] styles

--- client/src/components/routes/PageNotFound/PageNotFound.tsx
+++ client/src/components/routes/PageNotFound/PageNotFound.tsx
@@ -1,20 +1,17 @@
-import { Link, useRouteError } from 'react-router';
-
-interface IRouteError {
-  message?: string;
-}
+import { Link } from 'react-router';
 
 export const PageNotFound = () => {
-  const error = useRouteError() as IRouteError;
   return (
-    <>
-      <h1>Ой! Такой страницы нет</h1>
-      {error && (
-        <p>
-          <i>{error.message}</i>
-        </p>
-      )}
-      <Link to="/">Перейти на главную</Link>
-    </>
+    <div className="flex flex-col items-center my-24 gap-1.5 text-[#252b35]">
+      <h1 className="flex items-center text-3xl font-semibold gap-1.5">
+        Ой! Такой страницы нет <span className="text-4xl">&#128547;</span>
+      </h1>
+      <Link
+        to="/"
+        className="underline hover:scale-101 transition-transform duration-300 ease-in-out"
+      >
+        Перейти на главную
+      </Link>
+    </div>
   );
 };
