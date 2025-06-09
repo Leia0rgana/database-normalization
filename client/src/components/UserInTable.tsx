3286df7c1b721949b652f826c6cdd258af4fa849
Natalya Malyushenko <natamalyushenko@gmail.com>
2025-06-09 23:14:14 +0300

    [changed] refactoring

--- client/src/components/UserInTable.tsx
+++ client/src/components/UserInTable.tsx
@@ -12,18 +12,18 @@ export const UserInTable = (props: Props) => {
   const { userData, onDelete, onEdit } = props;
 
   return (
-    <tr className=" border-b border-gray-200 hover:bg-gray-100">
+    <tr className="text-lg border-b border-gray-200 hover:bg-gray-100">
       <td className="text-center py-3 px-5 truncate">{userData.name}</td>
       <td className="text-left py-3 px-5 truncate">{userData.email}</td>
       <td className="text-center py-3 px-5 truncate">********</td>
       <td className="flex justify-center gap-5">
         <VscEdit
-          className="mt-2 mx-0 text-xl cursor-pointer text-blue-500 hover:text-blue-700"
+          className="mt-4 mx-0 text-xl cursor-pointer text-blue-500 hover:text-blue-700"
           type="button"
           onClick={onEdit}
         />
         <AiOutlineDelete
-          className="mt-2 text-xl cursor-pointer text-red-500 hover:text-red-700"
+          className="mt-4 text-xl cursor-pointer text-red-500 hover:text-red-700"
           type="button"
           onClick={() => onDelete()}
         />
